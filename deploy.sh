ab6f31f71a9a7dad338ad4881ea34acf6cd2895f
Luca8991 <luca.berte98@hotmail.it>
2023-12-10 20:53:11 +0100

    chore: deploy and setup canister in one script

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,19 @@
 
 set -e
 
+echo "Deploying..."
 dfx deploy ic_side_services_backend --ic --argument '(variant { mainnet })'
+
+# make the canister fetch the ECDSA public key and store in its state
+echo "Setting up ECDSA public key in the canister..."
+dfx canister call --ic ic_side_services_backend set_canister_public_key
+
+# log addresses to see if everything went well
+echo "ZelId and ZelCash addresses on canister:"
+dfx canister call --ic ic_side_services_backend get_addresses --query
+
+# just log the status (controllers, balance, etc.)
+echo "Fetching canister status..."
+dfx canister status ic_side_services_backend --ic
+
+echo "Done!"
