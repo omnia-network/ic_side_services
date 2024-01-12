eb883165ea5988cbeae50dcda943781b6548c97a
Luca8991 <luca.berte98@hotmail.it>
2024-01-12 14:14:25 +0100

    refactor: rename orchestrator canister to proxy

--- ic-http-request-executor/src/canister/declarations/proxy_canister/index.js
+++ ic-http-request-executor/src/canister/declarations/proxy_canister/index.js
@@ -1,8 +1,8 @@
 import { Actor, HttpAgent } from "@dfinity/agent";
 
 // Imports and re-exports candid interface
-import { idlFactory } from "./orchestrator_canister.did.js";
-export { idlFactory } from "./orchestrator_canister.did.js";
+import { idlFactory } from "./proxy_canister.did.js";
+export { idlFactory } from "./proxy_canister.did.js";
 
 /* CANISTER_ID is replaced by webpack based on node environment
  * Note: canister environment variable will be standardized as
@@ -10,8 +10,8 @@ export { idlFactory } from "./orchestrator_canister.did.js";
  * beginning in dfx 0.15.0
  */
 export const canisterId =
-  process.env.CANISTER_ID_ORCHESTRATOR_CANISTER ||
-  process.env.ORCHESTRATOR_CANISTER_CANISTER_ID;
+  process.env.CANISTER_ID_PROXY_CANISTER ||
+  process.env.PROXY_CANISTER_CANISTER_ID;
 
 export const createActor = (canisterId, options = {}) => {
   const agent = options.agent || new HttpAgent({ ...options.agentOptions });
@@ -40,4 +40,4 @@ export const createActor = (canisterId, options = {}) => {
   });
 };
 
-export const orchestrator_canister = canisterId ? createActor(canisterId) : undefined;
+export const proxy_canister = canisterId ? createActor(canisterId) : undefined;
---
+++ src/proxy_canister/src/lib.rs
@@ -0,0 +1,5 @@
+use ic_cdk_macros::query;
+
+// thread_local! {
+//     /* flexible */ static REQUESTS: RefCell<OrchestratorCanister> = RefCell::new(OrchestratorCanister::new());
+// }
